0c53b0e015de985c66f8901d744012bb0068d698
Lucas Carvalho <67061874+lucasC-deOliveira@users.noreply.github.com>
2022-07-16 19:51:48 -0300

    Finalizando essa etapa do App

--- App.js
+++ App.js
@@ -1,8 +1,13 @@
 import React from "react";
 import { Home } from "./src/pages/Home";
+import { StatusBar } from "react-native";
 
 export default function App() {
   return (
-    <Home/>
-    )
+    <>
+      <StatusBar barStyle="light-content" />
+      <Home />
+    </>
+
+  )
 }
